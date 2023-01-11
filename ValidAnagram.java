242de2df3a667166889eb2e1a5c0131417807ce6
Gaurav Singh <gaurav.singh77722@gmail.com>
2023-01-11 13:51:49 -0800

    ValidAnagram Java Solution

---
+++ ValidAnagram.java
@@ -0,0 +1,20 @@
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<String,Integer> sHash = new HashMap<>();
+        HashMap<String,Integer> tHash = new HashMap<>();
+        for(String i:s.split("")){
+            sHash.put(i,sHash.getOrDefault(i,0) + 1);
+        }
+        for(String i:t.split("")){
+            tHash.put(i,tHash.getOrDefault(i,0) + 1);
+        }
+        
+        if(sHash.equals(tHash)){
+            return true;
+        }
+        return false;
+    }
+}
