f7d587f01ef2b83e0f2ce531ccb76b0fe3837a99
Gaurav Singh <gaurav.singh77722@gmail.com>
2023-01-11 13:26:29 -0800

    ContainsDuplicate Java Solution

---
+++ ContainsDuplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numsHS = new HashSet<>();
+        for(int i:nums){
+            numsHS.add(i);
+        }
+        if(numsHS.size()<nums.length){
+            return true;
+        }
+        return false;
+    }
+}
